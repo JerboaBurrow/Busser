14bd8e6e17d2d54a1fb331cd79013375e1ef31e9
Jerboa-app <dev@jerboa.app>
2024-03-06 19:13:49 +0000

    move content settings into ContentConfig

--- src/config.rs
+++ src/config.rs
@@ -30,127 +30,49 @@ pub const CONFIG_PATH: &str = "config.json";
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ThrottleConfig
 {
-    max_requests_per_second: f64,
-    timeout_millis: u128,
-    clear_period_seconds: u64
+    pub max_requests_per_second: f64,
+    pub timeout_millis: u128,
+    pub clear_period_seconds: u64
 }
 
-impl ThrottleConfig 
+/// Configure content settings
+/// - ```path```: path to site data
+/// - ```home```: path to home page served on /
+/// - ```allow_without_extension```: allow serving without .html
+/// - ```cache_period_seconds: u16```: page/resource max cache age
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ContentConfig
 {
-    pub fn get_max_requests_per_second(&self) -> f64
-    {
-        self.max_requests_per_second
-    }
-
-    pub fn get_timeout_millis(&self) -> u128
-    {
-        self.timeout_millis
-    }
-
-    pub fn get_clear_period_seconds(&self) -> u64
-    {
-        self.clear_period_seconds
-    }
+    pub path: String,
+    pub home: String,
+    pub allow_without_extension: bool,
+    pub cache_period_seconds: u16
 }
 
 /// Configure the server
 /// - ```port_https```: https port to serve on
 /// - ```port_http```: http port to serve on
-/// - ```path```: path to site data
-/// - ```home```: path to home page served on /
 /// - ```notification_endpoint```: currently unspported Discord webhook
 /// - ```cert_path```: ssl certificate
 /// - ```key_path```: ssl key
 /// - ```domain```: domain name for https redirect etc.
 /// - ```throttle```: [ThrottleConfig]
-/// - ```allow_without_extension```: allow serving without .html
 /// - ```stats```: [StatsConfig]
-/// - ```cache_period_seconds: u16```: page/resource max cache age
+/// - ```content```: [ContentConfig]
 /// - ```api_token```: token to use for the server's POST api
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Config
 {
-    port_https: u16,
-    port_http: u16,
-    path: String,
-    home: String,
-    notification_endpoint: Webhook,
-    cert_path: String,
-    key_path: String,
-    domain: String,
-    throttle: ThrottleConfig,
-    stats: StatsConfig,
-    allow_without_extension: bool,
-    cache_period_seconds: u16,
-    api_token: String
-}
-
-impl Config 
-{
-    pub fn get_port_http(&self) -> u16
-    {
-        self.port_http
-    }
-
-    pub fn get_port_https(&self) -> u16
-    {
-        self.port_https
-    }
-
-    pub fn get_end_point(&self) -> Webhook
-    {
-        self.notification_endpoint.clone()
-    }
-
-    pub fn get_cert_path(&self) -> String
-    {
-        self.cert_path.clone()
-    }
-
-    pub fn get_key_path(&self) -> String
-    {
-        self.key_path.clone()
-    }
-
-    pub fn get_path(&self) -> String
-    {
-        self.path.clone()
-    }
-
-    pub fn get_home(&self) -> String
-    {
-        self.home.clone()
-    }
-    
-    pub fn get_throttle_config(&self) -> ThrottleConfig
-    {
-        self.throttle.clone()
-    }
-
-    pub fn get_stats_config(&self) -> StatsConfig
-    {
-        self.stats.clone()
-    }
-
-    pub fn get_domain(&self) -> String
-    {
-        self.domain.clone()
-    }
-
-    pub fn get_allow_without_extension(&self) -> bool
-    {
-        self.allow_without_extension.clone()
-    }
-
-    pub fn get_cache_period_seconds(&self) -> u16
-    {
-        self.cache_period_seconds.clone()
-    }
-
-    pub fn get_api_token(&self) -> String
-    {
-        self.api_token.clone()
-    }
+    pub port_https: u16,
+    pub port_http: u16,
+    pub notification_endpoint: Webhook,
+    pub cert_path: String,
+    pub key_path: String,
+    pub domain: String,
+    pub throttle: ThrottleConfig,
+    pub stats: StatsConfig,
+    pub content: ContentConfig,
+    pub api_token: String
 }
 
 #[derive(Clone)]
