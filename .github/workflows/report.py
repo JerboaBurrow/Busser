9bb82bb555a1e3ee33f2853d774e2d49c0940422
Jerboa-app <dev@jerboa.app>
2024-04-27 06:59:33 +0100

    fix python

--- .github/workflows/report.py
+++ .github/workflows/report.py
@@ -9,14 +9,12 @@ parse = argparse.ArgumentParser(
 
 parse.add_argument("-m", required = True, action="store", dest = "main", help = "comparison branch name")
 parse.add_argument("-r", required = True, action="store", dest = "repo", help = "repository uri, e.g. \"github/JerboaBurrow/Busser\"")
+parse.add_argument("-h", required = False, action = "store_true", dest = "human", help = "adds additional humanised message about the result")
 
 args = parse.parse_args()
 
-repo = args["repo"]
-main = args["main"]
-
 status = requests.get(
-    url = f"https://coveralls.io/{repo}.json?branch={main}",
+    url = f"https://coveralls.io/{args.repo}.json?branch={args.main}",
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 )
 
@@ -48,10 +46,28 @@ for file in json.load(open('tarpaulin-report.json'))['files']:
 entries = sorted(entries, key = lambda x: x[1])
 
 this_coverage = round(overall/len(entries),2)
-output = "```\nTotal coverage {this_coverage} %"
 
+diff = None
 if main_coverage is not None:
     diff = this_coverage-main_coverage
+
+
+if args.human:
+    if diff > 0:
+        header = f"Looks like you increased coverage by {diff} %, fantastic!"
+    elif diff < 0:
+        header = f"Seems the coverage is below {main} by {diff} %, please consider adding to the tests, thanks!"
+    elif diff == 0:
+        header = f"Great, coverage is exactly the same as {main}!"
+    else:
+        header = ""
+    header += "\nHere is the full report breakdown\n\n"
+else:
+    header = ""
+
+output = f"```\n{header}Total coverage {this_coverage} %"
+
+if diff is not None:
     if diff < 0:
         sign = "-"
     elif diff > 0:
