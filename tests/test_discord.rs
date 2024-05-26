ae03d89bd4c54eb301b87f9b86c88a4f503124f3
Jerboa-app <dev@jerboa.app>
2024-05-26 11:13:52 +0100

    Adds try_post for Discord

--- tests/test_discord.rs
+++ tests/test_discord.rs
@@ -3,7 +3,7 @@ mod common;
 #[cfg(test)]
 mod discord
 {
-    use busser::integrations::{discord::post::post, webhook::Webhook};
+    use busser::integrations::{discord::post::post_message, webhook::Webhook};
 
     #[tokio::test]
     async fn test_webhook()
@@ -12,13 +12,13 @@ mod discord
 
         assert_eq!(w.get_addr(), "https://discord.com/api/webhooks/xxx/yyy");
 
-        assert!(post(&w, "400".to_string()).await.is_ok());
+        assert!(post_message(&w, &"400".to_string()).await.is_ok());
     }
 
     #[tokio::test]
     async fn test_err_webhook()
     {
         let w = Webhook::new("not_a_domain".to_string());
-        assert!(post(&w, "400".to_string()).await.is_err());
+        assert!(post_message(&w, &"400".to_string()).await.is_err());
     }
 }
